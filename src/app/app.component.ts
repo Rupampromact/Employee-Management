787a6104f8fa394c791d8acc1820e53392852c1c
Rupam Dhar <rupam@promact.com>
2018-02-06 10:53:40 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '<myapp>',
+  templateUrl: './employee.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
